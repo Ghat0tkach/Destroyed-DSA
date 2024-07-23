74e5dcdaf4c84eb61f7f18e1503a6196900a9628
Vikramaditya Singh <59855919+Ghat0tkach@users.noreply.github.com>
2024-07-24 04:25:08 +0530

    LeetBot added the following code

---
+++ KthLargest.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int length=nums.length;
+        Arrays.sort(nums);
+        return nums[length-k];
+    }
+}
+
+//optimal solution
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int num : nums){
+            minHeap.add(num);
+
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+}
