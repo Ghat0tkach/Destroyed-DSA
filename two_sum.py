ec6545e74ef6881a29ce3d3ca7a370fa4d2197c2
Vikramaditya Singh <59855919+Ghat0tkach@users.noreply.github.com>
2025-03-16 01:55:40 +0530

    Code uploaded by LeetBot

---
+++ two_sum.py
@@ -0,0 +1,22 @@
+def twoSum_bruteForce(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return None
+def twoSum_better(nums, target):
+    num_dict = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_dict:
+            return [num_dict[complement], i]
+        num_dict[num] = i
+    return None
+def twoSum_optimal(nums, target):
+    num_dict = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_dict:
+            return [num_dict[complement], i]
+        num_dict[num] = i
+    return None
