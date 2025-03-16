a33297a618f9367477085137ea57eab205ee03f7
Vikramaditya Singh <59855919+Ghat0tkach@users.noreply.github.com>
2025-03-16 22:46:57 +0530

    Code uploaded by LeetBot

---
+++ two_sum_test.py
@@ -0,0 +1,8 @@
+def two_sum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return None
